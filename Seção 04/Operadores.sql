005c8bc5c036ceefa035b950d81d00c7eeab3249
EandroC <eandrocleiton@gmail.com>
2024-05-03 20:52:47 -0300

    Exemplos sobre operadores lógicos

--- Seção 04/Operadores.sql
+++ Seção 04/Operadores.sql
@@ -4,6 +4,7 @@
 -- * (multiplicação): Multiplica dois valores.
 -- / (divisão): Divide o primeiro valor pelo segundo.
 -- % (módulo): Retorna o resto da divisão entre dois valores.
+
 -- Exemplo 01
 SELECT
 	email, 
@@ -44,6 +45,7 @@ FROM
 -- > (maior que): Verifica se o primeiro valor é maior que o segundo.
 -- <= (menor ou igual a): Verifica se o primeiro valor é menor ou igual ao segundo.
 -- >= (maior ou igual a): Verifica se o primeiro valor é maior ou igual ao segundo.
+
 -- Exemplo 01
 SELECT
 	customer_id,
@@ -52,3 +54,112 @@ SELECT
 	(professional_status= 'clt') AS cliente_clt
 FROM
 	sales.customers
+
+-- OPERADORES LÓGICOS: são usados para combinar ou negar condições em uma expressão.
+-- AND: Retorna verdadeiro se todas as condições forem verdadeiras.
+-- OR: Retorna verdadeiro se pelo menos uma das condições for verdadeira.
+-- LIKE: Compara valores com padrões de strings, útil para pesquisa flexível.
+-- BETWEEN: Verifica se um valor está dentro de um intervalo especificado.
+-- IS NULL: Verifica se um valor é nulo.
+-- IN: Verifica se um valor está presente em um conjunto de valores especificado.
+-- NOT: Negativo, usado para negar uma condição.
+
+-- Exemplo 01
+-- Os exemplos a seguir tem o mesmo resultado, mas com formas diferentes de escrita.
+-- Exemplo 01.a
+SELECT
+	*
+FROM
+	sales.products
+WHERE 
+	price>= 100000 AND price <=200000
+
+-- Exemplo 01.b
+SELECT
+	*
+FROM
+	sales.products
+WHERE 
+	price BETWEEN 100000 AND 200000
+
+-- Exemplo 02
+-- Os exemplos a seguir tem o mesmo resultado, mas com formas diferentes de escrita.
+-- Exemplo 02.a
+SELECT
+	*
+FROM
+	sales.products
+WHERE 
+	price < 100000 OR price > 200000
+
+-- Exemplo 02.b
+SELECT
+	*
+FROM
+	sales.products
+WHERE 
+	price NOT BETWEEN 100000 AND 200000
+
+-- Exemplo 03
+-- Os exemplos a seguir tem o mesmo resultado, mas com formas diferentes de escrita.
+-- Exemplo 03.a
+SELECT
+	*
+FROM
+	sales.products
+WHERE 
+	brand = 'HONDA' or brand = 'TOYOTA' or brand = 'RENAULT'
+
+-- Exemplo 03.b
+SELECT
+	*
+FROM
+	sales.products
+WHERE 
+	brand IN ('HONDA', 'TOYOTA', 'RENAULT')
+
+-- Exemplo 04
+-- Negação do exemplo 03
+SELECT
+	*
+FROM
+	sales.products
+WHERE 
+	brand NOT IN ('HONDA', 'TOYOTA', 'RENAULT')
+
+-- Exemplo 05
+-- Nomes que iniciam com ANA
+SELECT
+	DISTINCT first_name
+FROM
+	sales.customers
+WHERE 
+	first_name LIKE 'ANA%'
+
+-- Exemplo 06
+-- Nomes que terminam com ANA
+SELECT
+	DISTINCT first_name
+FROM
+	sales.customers
+WHERE 
+	first_name LIKE '%ANA'
+
+-- Exemplo 7
+-- Nomes que iniciam com ANA ou ana
+SELECT
+	DISTINCT first_name
+FROM
+	sales.customers
+WHERE 
+	first_name ILIKE 'ana%'
+
+-- Exemplo 8
+SELECT
+	*
+FROM
+	temp_tables.regions
+WHERE
+	population IS NULL
+
+	
