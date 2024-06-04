539418a65eb3186827ab3b89f2abfdeb5a2c6586
Eandro Cleiton <eandrocleiton@gmail.com>
2024-06-04 13:48:10 -0300

    Mudando localização de arquivo

--- Seção 08/01 - Subqueries.sql
+++ Seção 08/01 - Subqueries.sql
@@ -71,3 +71,54 @@ ORDER BY
 -- Subquerys no SELECT demoram muito, devem ser evitados quando possível.
 -- ORDER BY não pode ser usada internamente.
 
+-- MAIS EXEMPLOS WITH
+-- Exemplo 01
+WITH primeira_visita AS (
+	SELECT
+		customer_id,
+		MIN(visit_page_date) AS visita_1
+	FROM
+		sales.funnel
+	GROUP BY customer_id
+)
+SELECT
+	visit_page_date,
+	(fun.visit_page_date <> primeira_visita.visita_1) AS lead_recorrente,
+	COUNT(*)
+FROM sales.funnel AS fun
+LEFT JOIN
+	primeira_visita
+	ON
+	fun.customer_id = primeira_visita.customer_id
+GROUP BY
+	fun.visit_page_date, lead_recorrente
+ORDER BY
+	fun.visit_page_date DESC, lead_recorrente
+
+-- Exemplo 02
+WITH preco_medio AS (
+	SELECT
+		brand,
+		AVG(price) as preco_medio_da_marca
+	FROM
+		sales.products
+	GROUP BY
+		brand
+)
+SELECT
+	fun.visit_id,
+	fun.visit_page_date,
+	pro.brand,
+	(pro.price*(1+fun.discount)) AS preco_final,
+	preco_medio.preco_medio_da_marca,
+	(pro.price*(1+fun.discount)) - preco_medio.preco_medio_da_marca AS preco_vs_media
+FROM
+	sales.funnel AS fun
+LEFT JOIN
+	sales.products AS pro
+	ON
+	fun.product_id = pro.product_id
+LEFT JOIN
+	preco_medio
+	ON
+	pro.brand = preco_medio.brand
