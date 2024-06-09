63621d122920af34c14bf9e545e1c04f5447604c
Eandro Cleiton <eandrocleiton@gmail.com>
2024-06-09 19:35:12 -0300

    Exercício de subquery

---
+++ Seção 08/02 - Exercicios.sql
@@ -0,0 +1,15 @@
+-- EXERCÍCIOS ########################################################################
+
+-- (Exercício 1) Crie uma coluna calculada com o número de visitas realizadas por cada
+-- cliente da tabela sales.customers
+WITH qtd_visitas AS (
+	SELECT customer_id, COUNT(*) as n_visitas
+	FROM sales.funnel
+	GROUP BY customer_id
+)
+SELECT
+	cus.*,
+	n_visitas
+FROM sales.customers AS cus
+LEFT JOIN qtd_visitas AS vis
+	ON cus.customer_id = vis.customer_id
