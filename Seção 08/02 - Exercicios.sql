5de79e4a8e11bc75bde4ae68cf3c47cc7fe213fc
Eandro Cleiton <eandrocleiton@gmail.com>
2024-06-14 23:41:04 -0300

    Ajustando exercício

--- Seção 08/02 - Exercicios.sql
+++ Seção 08/02 - Exercicios.sql
@@ -1,4 +1,4 @@
--- EXERCÍCIOS ########################################################################
+-- EXERCÍCIOS ###################################################################
 
 -- (Exercício 1) Crie uma coluna calculada com o número de visitas realizadas por cada
 -- cliente da tabela sales.customers
