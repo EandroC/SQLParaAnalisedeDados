24325e179d1845e0f1fbd26910def9b6ca019588
Eandro Cleiton <eandrocleiton@gmail.com>
2024-06-18 20:09:07 -0300

    Exemplos sobre conversão de unidades

---
+++ Seção 09/01 - TratamentosDeDados.sql
@@ -0,0 +1,18 @@
+-- TRATAMENTO DE DADOS
+-- Conversão de Unidades
+-- Exemplo 01
+SELECT
+	'2021-10-01'::DATE - '2021-02-01'::DATE
+
+-- Exemplo 02
+SELECT
+	'100'::NUMERIC - '10'::NUMERIC
+
+-- Exemplo 03
+SELECT
+	REPLACE(112122::TEXT, '1', 'A')
+
+-- Caso o metodo '::' cause algum erro.
+-- Exemplo 04
+SELECT
+	CAST('2021-10-01' AS DATE) - CAST('2021-02-01' AS DATE)
